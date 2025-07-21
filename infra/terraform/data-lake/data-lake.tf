1b951bb9f837282b8958d3f3c6fde396d16190a4
Frank <Frankdt1234@gmail.com>
2025-07-21 12:12:04 -0400

    Batch commit from DLS

--- infra/terraform/data-lake/data-lake.tf
+++ infra/terraform/data-lake/data-lake.tf
@@ -26,7 +26,7 @@ resource "aws_s3_object" "lambda_zip" {
   bucket = "dls-lambda-functions-${var.region}"
   key    = "lambda.zip"
   source = "${path.module}/lambda.zip" 
-  etag   = filemd5("lambda.zip")
+  etag   = filemd5("ingestion_lambda/lambda.zip")
 }
 
 # Create ingestion Lambda
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   s3_bucket     = "dls-lambda-functions-${var.region}"
   s3_key        = "lambda.zip"
 
-  source_code_hash = filebase64sha256("lambda.zip")
+  source_code_hash = filebase64sha256("ingestion_lambda/lambda.zip")
 
   depends_on = [
     aws_s3_bucket.s3_bucket
