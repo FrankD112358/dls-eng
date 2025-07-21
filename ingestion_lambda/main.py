b904fbda8f12c336b2b648ccdbb6219b56dc2f0c
Frank <Frankdt1234@gmail.com>
2025-07-21 15:52:16 -0400

    Batch commit from DLS

--- ingestion_lambda/main.py
+++ ingestion_lambda/main.py
@@ -1,24 +1,28 @@
 import io
 import datetime
+import logging
 import boto3
 import pyarrow.json as paj
 import pyarrow.parquet as pq
 import pyarrow.csv as pacsv
-from logger import logger
 
 
 S3_CLIENT = boto3.client('s3')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+)
 
 
 def handler(event, context):
     try:
-        logger.info('event: %s\n context: %s', event, context)
+        logging.info('event: %s\n context: %s', event, context)
 
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = event['Records'][0]['s3']['object']['key']
         file_name = key.replace('staging/', '')
 
-        logger.info('bucket: %s\nkey: %s\nfile_name: %s', bucket, key, file_name)
+        logging.info('bucket: %s\nkey: %s\nfile_name: %s', bucket, key, file_name)
 
         s3_object = S3_CLIENT.get_object(Bucket=bucket, Key=key)
         raw_data = s3_object['Body'].read()
@@ -29,14 +33,14 @@ def handler(event, context):
         raw_path = f'raw/{partitioned_path}{file_name}'
         S3_CLIENT.put_object(Body=raw_data, Bucket=bucket, Key=raw_path)
 
-        logger.info('Put the raw data to %s successfully', raw_path)
+        logging.info('Put the raw data to %s successfully', raw_path)
 
         parquet_path = f"parquet/{partitioned_path}{file_name.replace(f'.{file_type}', '.parquet')}"
         S3_CLIENT.put_object(Body=parquet_body, Bucket=bucket, Key=parquet_path)
 
-        logger.info('Put parquet data to %s successfully', parquet_path)
+        logging.info('Put parquet data to %s successfully', parquet_path)
     except Exception as e:
-        logger.info('Error: %s', e)
+        logging.info('Error: %s', e)
 
 
 def convert(raw_data, file_type):
